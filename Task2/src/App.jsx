e2a4cad4238fa569d2dd24e96d7a23ec05ddb3ff
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-07-30 18:28:08 +0300

    Added sendRequest

--- Task2/src/App.jsx
+++ Task2/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState,useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-  const [exchangeRate,setExchangeRate] = useState(null)
+  const [exchangeRate, setExchangeRate] = useState(null);
+  const [value, setValue] = useState(null);
+  const[selectTo,setSelectTo] = useState(null);
+  const[selectFrom,setSelectFrom] = useState(null);
+  const[result,setResult] = useState(null);
 
-  useEffect( ()=>{
+  useEffect(() => {
     fetch("https://api.exchangerate.host/latest")
-    .then(response => response.json())
-    .then(data => setExchangeRate(Object.keys(data.rates)))
-    console.log(exchangeRate);
-  } )
+      .then((response) => response.json())
+      .then((data) => setExchangeRate(Object.keys(data.rates)));
+    
+  },[]);
+
+  const sendRequest = (e) => {
+    fetch(
+       `https://api.exchangerate.host/latest?BASE=${selectFrom}&symbols=${selectTo}&${value}=1`
+    )
+    .then((response) => response.json())
+    .then((data) => setResult(Object.keys(data.rates)))
+    .catch((err)=>(console.log(err)))
+    console.log(result)
+      
+    
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelectTo = (e) =>{
+    setSelectTo(e.target.value)
+    
+  }
+  const handleSelectFrom = (e) =>{
+    setSelectFrom(e.target.value)
+  }
+
+  
 
   return (
-    <>
-    <div >
 
-    
-    <select >
-      {exchangeRate?.map((exh)=>(
-        <option>{exh}</option>
+    <div>
+
+      <form>
+
+        <input type="number" onChange={handleChange}></input>
+
+        <select onChange={handleSelectTo}>
+          {exchangeRate?.map((exh,i) => (
+            <option key={i}>{exh}</option>
+          ))}
+        </select>
+
+        <select onChange={handleSelectFrom}>
+          {exchangeRate?.map((exh, i) => (
+            <option key={i}>{exh}</option>
+          ))}
+        </select>
         
-      ))}
-    </select>
+        <button type="button" onClick={sendRequest}>
+          Changee
+        </button>
+        <div>
+         {result}
+        </div>
+        
+
+      </form>
+
     </div>
-    
-    </>
-    
-    
-  )
+  );
 }
 
-export default App
+export default App;
