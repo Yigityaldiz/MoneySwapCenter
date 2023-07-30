f50c8a25c0dd66550fcaafb2931c0fa91d9f46f9
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-07-30 12:34:10 +0300

    Started Project

---
+++ Task2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React, {useState, useEffect} from 'react';
+import './App.css'
+
+function App() {
+ 
+  const []
+
+  return (
+    
+    
+  )
+}
+
+export default App
