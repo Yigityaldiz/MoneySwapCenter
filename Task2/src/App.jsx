4d34d8c7f7718d503b4c12f18dba2a6b05e4c99d
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-07-30 14:18:06 +0300

    Used fetch, useEffect , useState

--- Task2/src/App.jsx
+++ Task2/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import React, {useState, useEffect} from 'react';
+import { useState,useEffect} from 'react'
+
 import './App.css'
 
 function App() {
  
-  const []
+  const [exchangeRate,setExchangeRate] = useState(null)
+
+  useEffect( ()=>{
+    fetch("https://api.exchangerate.host/latest")
+    .then(response => response.json())
+    .then(data => setExchangeRate(Object.keys(data.rates)))
+    console.log(exchangeRate);
+  } )
 
   return (
+    <>
+    <div >
+
+    
+    <select >
+      {exchangeRate?.map((exh)=>(
+        <option>{exh}</option>
+        
+      ))}
+    </select>
+    </div>
+    
+    </>
     
     
   )
